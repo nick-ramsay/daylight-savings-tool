a6c62bfd92b755909c9c1a4361d8b462cfdd0adf
Nick Ramsay <NickRamsay1989@gmail.com>
2019-08-07 08:14:08 +1000

    Corections to script, reformatting of CSS, adding new IDs to HTML

--- dstScript.js
+++ dstScript.js
@@ -6,7 +6,7 @@ function currentDate() {
     var currentDay = dayNames[current.getDay()];
     var currentDate = current.getDate();
     var currentYear = current.getFullYear();
-    document.getElementById('currentDate').innerHTML = currentDay + ", " + currentDate + " " + currentMonth + ", " + currentYear
+    document.getElementById('currentDate').innerHTML = currentDay + ", " + currentDate + " " + currentMonth + " " + currentYear
 }
 
 function currentDstStatus() {
@@ -15,21 +15,21 @@ function currentDstStatus() {
     var dst_dom = dst_date.getDate(); //Day of Month
     var dst_dow = dst_date.getDay() + 1; //Day of Week, one added to make even with var dom
 
-    var dst_full_months = (dst_month > 9 && dst_month < 3) ? "true" : "false"; //Full months of November through March are always DST
-    var first_week = dst_dom <= 7 ? "true" : "false"; //True means it's still first week of month
-    var first_sunday = (first_week == "true" && dst_dow < dst_dom) ? "true" : "false"; //First Sunday of first week of month has passed
-    var first_sunday_april = (dst_month == 3 && first_sunday == "true") ? "true" : "false"; //Month is April and first Sunday has passed
-    var first_sunday_october = (dst_month == 9 && first_sunday == "true") ? "true" : "false"; //Month is October and first Sunday has passed
+    var dst_full_months = (dst_month > 9 && dst_month < 3) ? true: false; //Full months of November through March are always DST
+    var first_week = dst_dom <= 7 ? true : false; //True means it's still first week of month
+    var first_sunday = (first_week && dst_dow < dst_dom) ? true : false; //First Sunday of first week of month has passed
+    var first_sunday_april = (dst_month == 3 && first_sunday) ? true: false; //Month is April and first Sunday has passed
+    var first_sunday_october = (dst_month == 9 && first_sunday) ? true : false; //Month is October and first Sunday has passed
 
-    var dst = (dst_full_months == "true") || ((first_sunday_april == "false") && (first_sunday_october == "true")) ? "is" : "is not"; //If months are November through March OR it is before first Sunday of April and after first Sunday of October
+    var dst = (dst_full_months == "true") || ((!first_sunday_april) && (first_sunday_october)) ? "is" : "is not"; //If months are November through March OR it is before first Sunday of April and after first Sunday of October
     document.getElementById('currentDstStatus').innerHTML = dst
 }
 
 function dstStartDate() {
     var dstStartDt = new Date();
-    dstStartDt.setFullYear(2019);
-    dstStartDt.setMonth(3);
-    dstStartDt.setDate(1);
+        dstStartDt.setFullYear(dstStartDt.getFullYear());
+        dstStartDt.setMonth(3);
+        dstStartDt.setDate(1);
     var dstStartDtDOW = dstStartDt.getDay();
     var dstStartActualDate = (dstStartDtDOW > 0) ? (8 - dstStartDtDOW) : 1;
     dstStartDt.setDate(dstStartActualDate);
@@ -40,8 +40,10 @@ function dstStartDate() {
     var startDate = dstStartDt.getDate();
     var startMonth = monthNames[dstStartDt.getMonth()];
     var startYear = dstStartDt.getFullYear();
+    
     document.getElementById('dstStartDay').innerHTML = startDay
-    document.getElementById('dstStartDateMonth').innerHTML = startDate+" "+startMonth
+    document.getElementById('dstStartDate').innerHTML = startDate
+    document.getElementById('dstStartMonth').innerHTML = startMonth
     document.getElementById('dstStartYear').innerHTML = startYear
 }
 
@@ -60,7 +62,9 @@ function dstEndDate() {
     var endDate = dstEndDt.getDate();
     var endMonth = monthNames[dstEndDt.getMonth()];
     var endYear = dstEndDt.getFullYear();
+   
     document.getElementById('dstEndDay').innerHTML = endDay
-    document.getElementById('dstEndDateMonth').innerHTML = endDate+" "+endMonth
+    document.getElementById('dstEndDate').innerHTML = endDate
+    document.getElementById('dstEndMonth').innerHTML = endMonth
     document.getElementById('dstEndYear').innerHTML = endYear
 }
