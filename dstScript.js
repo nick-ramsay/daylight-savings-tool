8026f6218ecf0f2409347a40d0c0026f2396a3bf
Nick Ramsay <48420063+nick-ramsay@users.noreply.github.com>
2019-10-17 11:52:18 +1100

    Updated first_sunday var to !first_week

--- dstScript.js
+++ dstScript.js
@@ -19,7 +19,7 @@ function currentDstStatus() {
 
     var dst_full_months = (dst_month > 9 && dst_month < 3) ? true: false; //Full months of November through March are always DST
     var first_week = dst_dom <= 7 ? true : false; //True means it's still first week of month
-    var first_sunday = (first_week && dst_dow < dst_dom) ? true : false; //First Sunday of first week of month has passed
+    var first_sunday = (!first_week && dst_dow < dst_dom) ? true : false; //First Sunday of first week of month has passed
     var first_sunday_april = (dst_month == 3 && first_sunday) ? true: false; //Month is April and first Sunday has passed
     var first_sunday_october = (dst_month == 9 && first_sunday) ? true : false; //Month is October and first Sunday has passed
 
